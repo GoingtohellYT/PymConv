94f75c81da8be468b5f27c2252d8dc81265b4d72
Goingtohell <78041803+GoingtohellYT@users.noreply.github.com>
2023-09-28 14:35:06 +0200

    Update main.py
    adds the basics of the Tkinter ui

--- main.py
+++ main.py
@@ -1,8 +1,62 @@
 from folderConverter import folderContainer
 from pathlib import Path
+import tkinter
+import tkinter.filedialog
+
+path = ""
 
 
-test1 = folderContainer(Path('images'))
-# test1.see_images(end=1)
-test1.to_format('TIFF', 1)
+def open_folder():
+    """
+    Fonction qui ouvre un dossier avec tkinter.filedialog
 
+    Returns :
+        Le chemin de l'élément
+    Pré-condition :
+        Rien
+    Post-condition :
+        Affiche une interface pour sélectionner l'élément voulu et renvoie son chemin
+    """
+    global path = Path(tkinter.filedialog.askdirectory(mustexist=True))
+
+
+def open_file():
+    """
+    Fonction qui ouvre un fichier avec tkinter.filedialog
+
+    Returns :
+        Le chemin de l'élément
+    Pré-condition :
+        Rien
+    Post-condition :
+        Affiche une interface pour sélectionner l'élément voulu et renvoie son chemin
+    """
+    return Path(tkinter.filedialog.askopenfilename())
+
+
+def ui_window():
+    """
+     Fonction qui créé une interface graphique Tkinter pour l'application
+
+     Returns :
+        Rien
+     Pré-condition :
+        Aucune
+     Post-condition :
+        Une fenêtre s'affiche à l'écran
+    """
+    window = tkinter.Tk()
+    window.title("PymConv")
+    window.geometry("1080x720")
+    window.config(background="#B2BABB")
+
+    folder_button = tkinter.Button(window, text="Ouvrir un dossier", font=("Arial", 12), bg='white', command=open_folder)
+    folder_button.pack()
+
+    file_button = tkinter.Button(window, text="Ouvrir un fichier", font=("Arial", 12), bg='white', command=open_file)
+    file_button.pack()
+
+    window.mainloop()
+
+
+ui_window()
