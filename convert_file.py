bb8ec9b04dd8ae69e479964a4b47c4abdfa2500a
MATT-07 <117632549+MATT-07@users.noreply.github.com>
2023-09-28 20:38:33 +0200

    Update convert_file.py

--- convert_file.py
+++ convert_file.py
@@ -1,21 +1,19 @@
 from PIL import Image
+from pathlib import Path
 
 
-class FileContainer:
+class FilesContainers:
 
-    def __int__(self, img):
-        self.img = img  # nom de l'image et son extension en local avec le programme sous forme str
-        self.image = Image.open(self.img).convert("RGB") # image ouverte sur laquelle on va travailler
+    def __init__(self, paths):
+        self.paths = Path(os.path.realpath(paths))  # on modifie le chemin d'accès pour qu'il soit relatif
+        self.image = Image.open(self.paths).convert("RGB")  # image ouverte sur laquelle on va travailler
 
     def get_image(self):
         return self.image
 
-    def convert_img(self, format): # format de conversion sous forme str
-        self.image.save(self.img, format)
+    def show_image(self):
+        return self.image.show()
+
+    def convert_img(self, format_):  # format de conversion sous forme str
+        self.image.save(self.paths, format_)
         return self.image
-
-
-"""idées d'ajouts identifiés: 
-- gerer les options de qualité etc
--
-"""
