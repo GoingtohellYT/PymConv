a2e5325c377add3b41b4464c82172a33fa7f1115
MATT-07 <117632549+MATT-07@users.noreply.github.com>
2023-10-11 11:45:38 +0200

    Update convert_file.py

--- convert_file.py
+++ convert_file.py
@@ -2,10 +2,11 @@ from PIL import Image
 from pathlib import Path
 import os
 
+
 class FilesContainers:
 
     def __init__(self, paths):
-        self.paths = Path(os.path.realpath(paths))  # on modifie le chemin d'accès pour qu'il soit relatif
+        self.paths = paths  # on modifie le chemin d'accès pour qu'il soit relatif
         self.image = Image.open(self.paths).convert("RGB")  # image ouverte sur laquelle on va travailler
 
     def get_image(self):
@@ -15,6 +16,6 @@ class FilesContainers:
         return self.image.show()
 
     def convert_img(self, format_):  # format de conversion sous forme str
-        
-        new_path = image.split('.')[0]
+
+        new_path = os.path.splitext(self.paths)[0]
         self.image.save(new_path + f'.{format_.lower()}', format_)
